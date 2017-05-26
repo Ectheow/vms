cad36e8d51bf18fd5cfd8532b9186e276f68525b
John Phillips <jphillq@gmail.com>
2017-05-26 09:03:10 +0000

    update alistr-tools

--- alistr-tools.scm
+++ alistr-tools.scm
@@ -1,4 +1,4 @@
-(library (assq-tools)
+(library (alistr-tools)
   (export assqr aconsr)
   (import (rnrs))
   
@@ -6,54 +6,6 @@
     (or (null? alist)
 	(and (pair? alist)
 	     (pair? (car alist)))))
-  ;; (define (assqr keylist alol)
-  ;;   (cond
-  ;;    ((and (null? (cdr keylist))
-  ;; 	   (null? alol)) #false)
-  ;;    ((and (null? alol)
-  ;; 	   (not (null? (cdr keylist)))) #false)
-  ;;    ((and (alist? alol)
-  ;; 	   (alist? (cdr (car alol)))
-  ;; 	   (null? (cdr keylist)))
-  ;;     (cond
-  ;;      ((equal? (car keylist) (car (car alol)))
-  ;; 	(car alol))
-  ;;      (else
-  ;; 	(assqr keylist (cdr alol)))))
-  ;;    ((and (alist? alol)
-  ;; 	   (alist? (cdr (car alol)))
-  ;; 	   (not (null? (cdr keylist))))
-  ;;     (cond
-  ;;      ((equal? (car keylist)
-  ;; 		(car (car alol)))
-  ;; 	(let ((result-from-value
-  ;; 	       (assqr (cdr keylist)
-  ;; 			       (cdr (car alol)))))
-  ;; 	  (cond
-  ;; 	   ((eq? #false result-from-value)
-  ;; 	    (assqr keylist (cdr alol)))
-  ;; 	   (else
-  ;; 	    result-from-value))))
-  ;;      (else
-  ;; 	(assqr keylist (cdr alol)))))
-  ;;    ((and (alist? alol)
-  ;; 	   (not (alist? (cdr (car alol))))
-  ;; 	   (null? (cdr keylist)))
-  ;;     (cond
-  ;;      ((equal? (car keylist) (car (car alol)))
-  ;; 	(car alol))
-  ;;      (else
-  ;; 	(assqr keylist (cdr alol)))))
-  ;;    ((and (alist? alol)
-  ;; 	   (not (alist? (cdr (car alol))))
-  ;; 	   (not (null? (cdr keylist))))
-  ;;     (cond
-  ;;      ((equal? (car keylist) (car (car alol)))
-  ;; 	(let ((found-in-value (assqr (cdr keylist) (cdr (car alol)))))
-  ;; 	  (cond
-  ;; 	   ((not (eq? #false found-in-value)) found-in-value)
-  ;; 	   (else (assqr keylist (cdr alol))))))
-  ;;      (else (assqr keylist (cdr alol))))))))
 
   (define (assqr keylist alol)
     (cond
@@ -91,18 +43,9 @@
 		  "Parameters do not meet data definition"
 		  keylist alol))))
 
-  (define (aconsr keylist value alol)
+    (define (aconsr keylist value alol)
     (cond
-     ((and (null? (cdr keylist))
-	   (null? alol))
-      (cons (cons (car keylist) value) '()))
-     ((and (null? (cdr keylist))
-	   (and (alist? alol)
-		(alist? (cdar alol))))
-      (cons (cons (car keylist) value) alol))
-     ((and (null? (cdr keylist))
-	   (and (alist? alol)
-		(not (alist? (cdar alol)))))
+     ((null? (cdr keylist))
       (cons (cons (car keylist) value) alol))
      ((and (not (null? (cdr keylist)))
 	   (null? alol))
@@ -130,5 +73,5 @@
 	     (error 'aconsr "keypath not in alist"
 		    alol keylist))
 	    (else (cons (car alol)
-			(aconsr keylist value (cdr alol))))))))
-  )
+			(aconsr keylist value (cdr alol)))))))))
+
